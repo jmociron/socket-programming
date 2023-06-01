218e02ec7d6020a6304963fb9d020108e59bb7f2
Jamie Mari Ciron <jociron@up.edu.ph>
2023-06-01 15:24:10 +0800

    feat: changed n

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ config.read("config.ini")
 host = config.get("Connect", "host")
 port = config.getint("Connect", "port")
 
-n = 1001
-c = 2
+n = 4001
+c = 4
 client_count = c
 matrix = assign_values([[0 for x in range(n)] for y in range(n)])
 # print_matrix(matrix)
